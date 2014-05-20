e3d3769077f8dbdc28e8a9a9387d3d8507c8a4f2
otomoringo <otomoringo.sour@gmail.com>
2014-05-20 12:34:51 +0900

    Add latest parser.

--- src/Parser.java
+++ src/Parser.java
@@ -3,24 +3,28 @@ import java.util.Stack;
 
 public class Parser {
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 			String line = sc.nextLine();
 			if (line.equals("0"))
 				break;
-			ForParser(line);
+			String result = parse(line);
+			// String result = ifParse(line);
+			System.out.println(result);
 		}
 		sc.close();
 	}
 
-	private static void ForParser(String line) {
+	public static String parse(String line) {
+		String result = "";
 		Stack<Character> stack = new Stack<Character>();
 		int count = 0;
-		int roop_counter = 0;
+		int loopCounter = 1;
 
 		for (int i = 0; i < line.length(); i++) {
 			char ch = line.charAt(i);
+			if (loopCounter == 0 && stack.empty())
+				break;
 			switch (ch) {
 			case '(':
 				stack.push(ch);
@@ -28,22 +32,57 @@ public class Parser {
 				break;
 
 			case ')':
-				i -= count;
-				roop_counter--;
+				i -= count + 1;
+				loopCounter--;
+				count = 0;
 				break;
 
 			default:
-				if (stack.peek() == '(') {
-					roop_counter = (int) ch;
+				if (!stack.empty() && stack.peek() == '(') {
+					loopCounter = Integer.parseInt("" + ch);
 					stack.pop();
 				} else {
-					System.out.println(ch);
+					result += ch;
+					count++;
 				}
+				break;
 			}
-
 		}
+		return result;
 	}
 
+	public static String ifParse(String line) {
+		String result = "";
+		Stack<Character> stack = new Stack<Character>();
+		int random = 0;
+
+		for (int i = 0; i < line.length(); i++) {
+			char ch = line.charAt(i);
+			switch (ch) {
+			case '[':
+				stack.push(ch);
+				break;
+
+			case '|':
+				stack.pop();
+				stack.push(ch);
+				break;
+
+			case ']':
+				stack.pop();
+				break;
+
+			default:
+				if (random == 1 && stack.peek() == '[') {
+					result += ch;
+				} else if (random == 0 && stack.peek() == '|') {
+					result += ch;
+				}
+				break;
+			}
+		}
+		return result;
+	}
 	// case '[':
 	// stack.push(ch);
 	// // stack.empty();
